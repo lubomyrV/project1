f700c9e95148dfc2b6291b868140888a4ad3a7f9
lubomyrV <lubomyr.voloschak@gmail.com>
2017-11-19 14:41:23 +0200

    test comit

--- src/main/java/com/example/project/controllers/AdminControll.java
+++ src/main/java/com/example/project/controllers/AdminControll.java
@@ -23,9 +23,7 @@ public class AdminControll {
     private StorageService storageService;
 
     private int elementsOnPage = 2;
-
     private String sortPages = "";
-
     private String defaultPath = new File("").getAbsolutePath()+File.separator+"src"+File.separator+"main"+File.separator+"resources"+File.separator+"static"+File.separator+"images"+File.separator;
 
     @GetMapping("/admin")
@@ -150,7 +148,6 @@ public class AdminControll {
         } else {
             realPath = product.getRealPath();
         }
-
         productService.updateProduct(id,model,price,description,image,realPath);
         return "redirect:/admin";
     }
@@ -201,6 +198,5 @@ public class AdminControll {
     public String login (){
         return "/login";
     }
-
 
 }
