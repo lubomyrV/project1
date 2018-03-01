faa27ffa37defa2d540b8a2f5a92cf5355f3e1c9
lubomyrV <lubomyr.voloschak@gmail.com>
2018-03-01 11:25:55 +0200

    add orders

---
+++ src/main/java/com/example/project/models/Order.java
@@ -0,0 +1,15 @@
+//package com.example.project.models;
+//
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//
+//@Entity
+//public class Order {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private int id;
+//
+//}
